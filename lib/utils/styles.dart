b9279236aa1e6e09bc1aabe27b003f2470709478
wesam <wesamalessa33@gmail.com>
2022-07-30 17:45:49 +0200

    test 7

---
+++ lib/utils/styles.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/utils/dimensions.dart';
+
+final robotoRegular = TextStyle(
+  fontFamily: "Roboto",
+  fontWeight: FontWeight.w400,
+  fontSize: Dimensions.font26,
+);
+
+final robotoMedium = TextStyle(
+  fontFamily: "Roboto",
+  fontWeight: FontWeight.w500,
+  fontSize: Dimensions.font26,
+);
+
+final robotoBold = TextStyle(
+  fontFamily: "Roboto",
+  fontWeight: FontWeight.w700,
+  fontSize: Dimensions.font26,
+);
+
+final robotoBlack = TextStyle(
+  fontFamily: "Roboto",
+  fontWeight: FontWeight.w900,
+  fontSize: Dimensions.font26,
+);
